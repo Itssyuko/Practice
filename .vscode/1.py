9427b787dd3262dc07d8b9272d21250330712569
itssyuu <lovelabi@sina.com>
2018-09-06 21:01:53 +0800

    学习

--- .vscode/1.py
+++ .vscode/1.py
@@ -1,2 +1,8 @@
-print("Hello World")
-print("I am a fool")
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+print(my_abs(-99))
