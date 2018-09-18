72df750ed25de02a1fbbffbf0114b005f92bdfca
itssyuu <lovelabi@sina.com>
2018-09-18 11:05:43 +0800

    合理利用递归

--- test3.py
+++ test3.py
@@ -1,8 +1,29 @@
-#/usr/bin/env python3
-# -*- coding:utf-8 -*-
-def power(x, n=2):
-    s = 1
-    while n > 0:
-        n = n - 1
-        s = s * x
+#/usr/bin/env python
+#-*- coding: utf-8 -*-
+#when u think of this problem,first u should figure out how many cases are there
+# 1.no space in this string
+# 2. have some spaces at the beginning or some spaces at the end of the string,or both
+# 3. the string consists of a number of spaces
+def trim(s):
+   flag = 1
+   while flag == 1:
+       if s[:1] == ' ':
+           s = s[1:]
+        elif s[-1:] == ' ':
+            s = s[:-1]
     return s
+
+if trim('hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello') != 'hello':
+    print('测试失败!')
+elif trim('  hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello  world  ') != 'hello  world':
+    print('测试失败!')
+elif trim('') != '':
+    print('测试失败!')
+elif trim('    ') != '':
+    print('测试失败!')
+else:
+    print('测试成功!')
